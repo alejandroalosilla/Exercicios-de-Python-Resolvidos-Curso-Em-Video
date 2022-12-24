37ca30179f01564f49d6a4d107793d1b91fc0afd
Alejandro Alosilla <alejandroalosilla@gmail.com>
2022-12-23 22:33:59 -0300

    1

---
+++ ex000/ex000.py
@@ -0,0 +1,4 @@
+# Ex000 - Crie um programa que escreva "Olá, Mundo!" na tela.
+
+msg = ('{}Olá, Mundo!'.format('\033[31m'))
+print(msg)
--- ex001/ex000.py
+++
@@ -1,4 +0,0 @@
-# Crie um programa que escreva "Olá, Mundo!" na tela.
-
-msg = ('{}Olá, Mundo!'.format('\033[31m'))
-print(msg)
